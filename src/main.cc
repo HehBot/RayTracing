19b6bb66530d83b0f7182f06eb7c2474d8277023
HehBot <razor7827@gmail.com>
2023-05-03 18:36:06 +0530

    Added multithreaded rendering

--- src/main.cc
+++ src/main.cc
@@ -7,8 +7,10 @@
 #include "read_from_json.h"
 #include "vec3.h"
 
+#include <chrono>
 #include <cmath>
 #include <iostream>
+#include <thread>
 #include <vector>
 
 static metadata m;
@@ -39,19 +41,35 @@ int main(int argc, char* argv[])
     hittable_list world;
     camera* cam = read_from_json(argv[1], m, world);
 
+    auto t1 = std::chrono::high_resolution_clock::now();
+
     std::vector<color> image(m.width * m.height, { 0.0, 0.0, 0.0 });
-    for (int j = 0; j < m.height; ++j) {
-        for (int i = 0; i < m.width; ++i) {
+
+    std::vector<std::thread> threads;
+    int const N = std::thread::hardware_concurrency();
+
+    auto job = [&](int const tid) {
+        for (int c = tid; c < m.height * m.width; c += N) {
+            int i = c % m.width;
+            int j = c / m.width;
             color accumulator(0.0, 0.0, 0.0);
             for (int k = 0; k < m.samples_per_pixel; ++k) {
                 double u = (i + random_double()) / m.width;
                 double v = (j + random_double()) / m.height;
                 accumulator += ray_color(cam->get_ray(u, v), world, m.max_depth);
             }
-            image[m.width * j + i] = accumulator / m.samples_per_pixel;
+            image[c] = accumulator / m.samples_per_pixel;
         }
-    }
+    };
+
+    for (int i = 0; i < N; ++i)
+        threads.emplace_back(job, i);
+    for (auto& th : threads)
+        th.join();
 
     write_to_img_file(argv[2], img_format::PPM, m.width, m.height, image);
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::cerr << "Done, time taken = " << std::chrono::duration<double, std::milli>(t2 - t1).count() / 1000.0 << " s using " << N << " threads\n";
     return 0;
 }
